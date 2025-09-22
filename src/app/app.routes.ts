43bb01126623f01eeb308d1a517de2d951ec82a3
Vitor Lopes Walendowsky <043347@PROWAY.TREINA>
2025-09-22 20:53:08 -0300

    routes corrigido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { Calculadora } from './Components/calculadora/calculadora';
 import { ListaPessoas } from './Components/lista-pessoas/lista-pessoas';
 import { CalculadoraRetangulo } from './Components/calculadora-retangulo/calculadora-retangulo';
+import { CadastroAluno } from './Components/cadastro-aluno/cadastro-aluno';
+import { ListaAlunosComponent } from './Components/lista-alunos/lista-alunos';
 
 export const routes: Routes = [
     {path: "calculadora", component: Calculadora},
     {path: "lista-pessoas", component: ListaPessoas},
-    {path: "calculadora-retangulo", component: CalculadoraRetangulo}
-    
+    {path: "calculadora-retangulo", component: CalculadoraRetangulo},
+    {path: "alunos", component: ListaAlunosComponent},
+    {path: "alunos/cadastro", component: CadastroAluno}
 ];
 //SPA: Single Page Application
