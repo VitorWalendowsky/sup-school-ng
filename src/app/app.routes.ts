e2979b5b0721a1f54fc3f66b06df613cec28111f
Vitor Lopes Walendowsky <043347@proway.treina>
2025-10-01 18:52:27 -0300

    corrigdo falhas do cód.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
 import { Calculadora } from './Components/calculadora/calculadora';
 import { ListaPessoas } from './Components/lista-pessoas/lista-pessoas';
-import { CalculadoraRetangulo } from './Components/calculadora-retangulo/calculadora-retangulo';
 import { CadastroAluno } from './Components/cadastro-aluno/cadastro-aluno';
 import { ListaAlunosComponent } from './Components/lista-alunos/lista-alunos';
 import { CadastroTreino } from './Components/cadastro-treino/cadastro-treino';
 import { CadastroTurmas } from './Components/cadastro-turmas/cadastro-turmas';
+import { CalculadoraRetanguloComponent } from './Components/calculadora-retangulo/calculadora-retangulo.component';
 
 export const routes: Routes = [
     {path: "calculadora", component: Calculadora},
     {path: "lista-pessoas", component: ListaPessoas},
-    {path: "calculadora-retangulo", component: CalculadoraRetangulo},
+    {path: "calculadora-retangulo", component: CalculadoraRetanguloComponent},
     {path: "alunos", component: ListaAlunosComponent},
   {path: "alunos/cadastro", component: CadastroAluno},
   { path: '', redirectTo: 'cadastro-treino', pathMatch: 'full' },
